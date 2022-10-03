bceaab91a30135d2c39d3f28c7106ed014f6240d
Ruben Arushanyan <ruben.arushanyan24@gmail.com>
2022-10-03 18:23:47 +0400

    Add Home Page features

--- website/src/components/HomepageFeatures/index.js
+++ website/src/components/HomepageFeatures/index.js
@@ -3,9 +3,14 @@ import clsx from 'clsx';
 import styles from './styles.module.css';
 
 const FeatureList = [
+  // {
+  //   title: 'Flexible',
+  //   Svg: require('@site/static/images/features/flexible.svg').default,
+  //   description: <></>,
+  // },
   {
-    title: 'Flexible',
-    Svg: require('@site/static/images/features/flexible.svg').default,
+    title: 'Encapsulated',
+    Svg: require('@site/static/images/features/encapsulated.svg').default,
     description: <></>,
   },
   {
@@ -14,50 +19,45 @@ const FeatureList = [
     description: <></>,
   },
   {
-    title: 'Powerful',
-    Svg: require('@site/static/images/features/powerful.svg').default,
-    description: <></>,
-  },
-  {
-    title: 'Effective',
-    Svg: require('@site/static/images/features/effective.svg').default,
-    description: <></>,
-  },
-  {
-    title: 'Secure',
-    Svg: require('@site/static/images/features/secure.svg').default,
-    description: <></>,
-  },
-  {
-    title: 'Debuggable',
-    Svg: require('@site/static/images/features/debuggable.svg').default,
-    description: <></>,
-  },
-  {
-    title: 'Encapsulated',
-    Svg: require('@site/static/images/features/encapsulated.svg').default,
-    description: <></>,
-  },
-  {
-    title: 'Optimized',
-    Svg: require('@site/static/images/features/optimized.svg').default,
-    description: <></>,
-  },
-  {
     title: 'Fast',
     Svg: require('@site/static/images/features/fast.svg').default,
     description: <></>,
   },
-  {
-    title: 'Predictable',
-    Svg: require('@site/static/images/features/predictable.svg').default,
-    description: <></>,
-  },
-  {
-    title: 'Intuitive',
-    Svg: require('@site/static/images/features/intuitive.svg').default,
-    description: <></>,
-  },
+  // {
+  //   title: 'Powerful',
+  //   Svg: require('@site/static/images/features/powerful.svg').default,
+  //   description: <></>,
+  // },
+  // {
+  //   title: 'Effective',
+  //   Svg: require('@site/static/images/features/effective.svg').default,
+  //   description: <></>,
+  // },
+  // {
+  //   title: 'Secure',
+  //   Svg: require('@site/static/images/features/secure.svg').default,
+  //   description: <></>,
+  // },
+  // {
+  //   title: 'Debuggable',
+  //   Svg: require('@site/static/images/features/debuggable.svg').default,
+  //   description: <></>,
+  // },
+  // {
+  //   title: 'Optimized',
+  //   Svg: require('@site/static/images/features/optimized.svg').default,
+  //   description: <></>,
+  // },
+  // {
+  //   title: 'Predictable',
+  //   Svg: require('@site/static/images/features/predictable.svg').default,
+  //   description: <></>,
+  // },
+  // {
+  //   title: 'Intuitive',
+  //   Svg: require('@site/static/images/features/intuitive.svg').default,
+  //   description: <></>,
+  // },
 ];
 
 function Feature({Svg, title, description}) {
